91876a92160b09aff58b875d0d55e591590efd2e
Maxim <mrgamer594@gmail.com>
2024-02-16 17:47:39 +0200

    add mongoose model

---
+++ models/contactsModel.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
